765a009feb28a2f8eee1b174784937d6c615c3d4
rbs5340 <rbs5340@g.rit.edu>
2025-04-23 01:51:32 -0400

    DomoMakerD Done 2

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,36 +1,36 @@
-const requiresLogin = (req,res,next)=>{
-    if(!req.session.account){
-        return res.redirect('/');
-    }
+const requiresLogin = (req, res, next) => {
+  if (!req.session.account) {
+    return res.redirect('/');
+  }
 
-    return next();
-}
+  return next();
+};
 
-const requiresLogout=(req,res,next)=>{
-    if(req.session.account){
-        return res.redirect('/maker');
-    }
+const requiresLogout = (req, res, next) => {
+  if (req.session.account) {
+    return res.redirect('/maker');
+  }
 
-    return next();
-}
+  return next();
+};
 
-const requiresSecure = (req,res,next)=>{
-    if(req.headers['x-forwarded-proto']!=='https'){
-        return res.redirect(`https://${req.hostname}${req.url}`);
-    }
+const requiresSecure = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(`https://${req.hostname}${req.url}`);
+  }
 
-    return next();
-}
+  return next();
+};
 
-const bypassSecure = (req,res,next)=>{
-    next();
-}
+const bypassSecure = (req, res, next) => {
+  next();
+};
 
-module.exports.requiresLogin=requiresLogin;
-module.exports.requiresLogout=requiresLogout;
+module.exports.requiresLogin = requiresLogin;
+module.exports.requiresLogout = requiresLogout;
 
-if(process.env.NODE_ENV === 'production'){
-    module.exports.requiresSecure=requiresSecure;
-}else{
-    module.exports.requiresSecure=bypassSecure;
+if (process.env.NODE_ENV === 'production') {
+  module.exports.requiresSecure = requiresSecure;
+} else {
+  module.exports.requiresSecure = bypassSecure;
 }
